4965a32b1eae354e4b2707af05d7fa9b6a3e514d
Mouad Hachemi <mouad.hachemi2k@gmail.com>
2025-07-07 01:16:20 +0100

    First commit: base telegram bot with order command

---
+++ README.md
@@ -0,0 +1,1 @@
+# lgzlllkt-telegram-bot
---
+++ index.js
@@ -0,0 +1,87 @@
+import { Telegraf } from "telegraf";
+import dotenv from "dotenv";
+
+dotenv.config();
+const orderState = {};
+
+let bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.start(ctx => ctx.reply("📚 مرحبا بك في متجر الغزال للكتب، لتسجيل طلب أدخل: \n/talab"));
+
+bot.command("talab", async (ctx) => {
+  const userId = ctx.from.id;
+  orderState[userId] = {
+    step: "awaiting_title",
+  };
+  ctx.reply("📝 ماهو عنوان الكتاب الذي تريده؟");
+});
+
+bot.on("text", async (ctx) => {
+  const userId = ctx.from.id;
+  const userInput = ctx.message.text;
+  
+  const state = orderState[userId];
+  if (!state) {
+    return ctx.reply("❗أعتذر، لم أتعرف على الأمر المطلوب، إليك قائمة الأوامر المُتاحة:\n/talab\n/bahth\n/info");
+  }
+  
+  switch(state.step) {
+    case "awaiting_title": {
+      state.bookTitle = userInput;
+      state.step = "awaiting_quantity";
+      ctx.reply("📦 كم من نسخة؟");
+      break;
+    }
+    case "awaiting_quantity": {
+      if (!/^\d+$/.test(userInput)) {
+        return ctx.reply("❌ من فضلك أدخِل رقما صحيحا.");
+      } else {
+        state.quantity = parseInt(userInput);
+        state.step = "awaiting_name";
+        ctx.reply("👤 ما إسمك الكامل؟ ");
+        break;
+      }
+    } 
+    case "awaiting_name": {
+      state.name = userInput;
+      state.step = "awaiting_phone";
+      ctx.reply("📱ما رقم هاتفك؟");
+      break;
+    }
+    case "awaiting_phone": {
+      state.phone = userInput;
+      state.step = "awaiting_address";
+      ctx.reply("🚚 ما عنوان التسليم الخاص بك؟");
+      break;
+    }
+    case "awaiting_address": {
+      state.address = userInput;
+      state.step = "done";
+      ctx.reply(
+      `
+      ✅ تم تسجيل الطلب:\n` +
+        `📚 *الكتاب:* ${state.bookTitle}\n` +
+        `🔢 *الكمية:* ${state.quantity}\n` +
+        `👤 *الإسم:* ${state.name}\n` +
+        `📱 *رقم الهاتف:* ${state.phone}\n` +
+        `🏠 *عنوان التسليم:* ${state.address}
+      `, {parse_mode: "Markdown"}
+      )
+      delete orderState[userId];
+      break;
+    }
+    default: {
+      ctx.reply("⚠️ لتسجيل طلب أدخل: \n /talab");
+    }
+  }
+});
+
+bot.launch();
+
+process.once("SIGINT", () => {
+  bot.stop("SIGINT");
+});
+
+process.once("SIGTERM", () => {
+  bot.stop("SIGTERM");
+});
